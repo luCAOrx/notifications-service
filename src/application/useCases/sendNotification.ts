5c3c64ef6a4303981f0a749941e6e2d67966b3b4
luCAOrx <lucaorx@protonmail.com>
2022-12-18 04:46:00 -0300

    chore: Domain,use cases and business rules

---
+++ src/application/useCases/sendNotification.ts
@@ -0,0 +1,37 @@
+import { NotificationsRepository } from '../repositories/notificationRepository';
+import { Content } from '../entities/content';
+import { Notification } from '../entities/notification';
+import { Injectable } from '@nestjs/common';
+
+interface SendNotificationRequest {
+  recipientId: string;
+  content: string;
+  category: string;
+}
+
+interface SendNotificationResponse {
+  notification: Notification;
+}
+
+@Injectable()
+export class SendNotification {
+  constructor(private notificationRepository: NotificationsRepository) {}
+
+  async execute(
+    request: SendNotificationRequest,
+  ): Promise<SendNotificationResponse> {
+    const { category, content, recipientId } = request;
+
+    const notification = new Notification({
+      category,
+      content: new Content(content),
+      recipientId,
+    });
+
+    await this.notificationRepository.create(notification);
+
+    return {
+      notification,
+    };
+  }
+}
