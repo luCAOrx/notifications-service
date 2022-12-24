92bc17630d87607edbc13ac95f0a88f97ccf298c
luCAOrx <lucaorx@protonmail.com>
2022-12-24 05:06:16 -0300

    chore: Changed broker host, added producer warn correction, formatted producer.send() method and added log when message is created

--- src/infra/messaging/kafka/kafkaProducer.ts
+++ src/infra/messaging/kafka/kafkaProducer.ts
@@ -1,28 +1,32 @@
-import { Kafka } from 'kafkajs';
+import { Kafka, Partitioners } from 'kafkajs';
 import { randomUUID } from 'node:crypto';
 
 async function bootstrap() {
   const kafka = new Kafka({
     clientId: 'notifications',
-    brokers: ['172.24.0.2:9092'],
+    brokers: ['127.0.0.1:9092'],
   });
 
-  const producer = kafka.producer();
+  const producer = kafka.producer({
+    createPartitioner: Partitioners.LegacyPartitioner,
+  });
 
   await producer.connect();
 
-  await producer.send({
-    topic: 'notifications.send-notification',
-    messages: [
-      {
-        value: JSON.stringify({
-          content: 'Hello world',
-          category: 'kafka',
-          recipientId: randomUUID(),
-        }),
-      },
-    ],
-  });
+  await producer
+    .send({
+      topic: 'notifications.send-notification',
+      messages: [
+        {
+          value: JSON.stringify({
+            content: 'Hello world',
+            category: 'kafka',
+            recipientId: randomUUID(),
+          }),
+        },
+      ],
+    })
+    .then(() => console.log('Created message.'));
 
   await producer.disconnect();
 }
