86e6d1e01894e0c476a660ecb36cc0e7a26fa489
luCAOrx <lucaorx@protonmail.com>
2023-01-04 02:40:19 -0300

    test: Add information for functions and function for sending notification in infra layer

--- test/factories/notificationFactory.ts
+++ test/factories/notificationFactory.ts
@@ -3,9 +3,19 @@ import {
   Notification,
   NotificationProps,
 } from '@application/entities/notification';
+import { INestApplication } from '@nestjs/common';
+
+import * as request from 'supertest';
+
+import { randomUUID } from 'node:crypto';
 
 type Override = Partial<NotificationProps>;
 
+/**
+ * @info
+ *
+ * This function is used for create a notification in domain layer
+ **/
 export function makeNotification(override: Override = {}) {
   return new Notification({
     category: 'social',
@@ -13,4 +23,28 @@ export function makeNotification(override: Override = {}) {
     recipientId: 'recipient-1',
     ...override,
   });
+}
+
+/**
+ * @info
+ *
+ * This function is used for send a notification in infra layer
+ **/
+export function sendNotification({
+  override = {},
+  server,
+}: {
+  override?: Override;
+  server: INestApplication;
+}) {
+  const recipientId = randomUUID();
+
+  return request(server)
+    .post('/notifications')
+    .send({
+      content: 'This is a integration test notification',
+      category: 'integration test',
+      recipientId,
+      ...override,
+    });
 }
