3f4b7aed0b56d70684d83b888d6783286e99e517
luCAOrx <lucaorx@protonmail.com>
2023-01-06 05:25:16 -0300

    docs: Added settings to initialize swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { KafkaConsumerService } from '@infra/messaging/kafka/kafkaConsumer.servi
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions } from '@nestjs/microservices';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -10,6 +11,18 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   const kafkaConsumerService = app.get(KafkaConsumerService);
+
+  const config = new DocumentBuilder()
+    .setTitle('Notifications service')
+    .setDescription('This API aims to manage notifications')
+    .setVersion('0.0.1')
+    .addServer('http://localhost:3000', 'Development API')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document, {
+    customSiteTitle: 'Notifications service API documentation',
+  });
 
   app.connectMicroservice<MicroserviceOptions>({
     strategy: kafkaConsumerService,
